b62c665d769d5735623182bae7f3c4cf0bfa1a8b
Cem Tosun <134544650+Kilopi3142@users.noreply.github.com>
2023-07-27 12:33:11 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+	  
+	  
+	  
+	  <h1>Kilopi Tutorial Videos</h1>
+
+<div className="Appx">
+	  
+
+<figure className="snip1361">
+
+	  <iframe src="https://www.youtube.com/embed/PEH-CdLoR0g" title="A"
+		width = "400" height = "250"
+		allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+<iframe src="https://www.youtube.com/embed/2YOoaD2py7k" title="A" 
+	  width = "400" height = "250"
+	  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+<iframe src="https://www.youtube.com/embed/6qQueqwMiVE" title="A" 
+	  width = "400" height = "250"
+	  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+<iframe src="https://www.youtube.com/embed/iTRM7s4cqXY" title="A"
+		width = "400" height = "250"
+		allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+<iframe src="https://www.youtube.com/embed/bXCHdGYra6o" title="A" 
+	  width = "400" height = "250"
+	  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+<iframe src="https://www.youtube.com/embed/q89sXxLtmp4" title="A" 
+	  width = "400" height = "250"
+	  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+</figure>
+
+	  
+	  
+	  
+	  </div>
+	  
+	  
+	  
+	
+
+
+
+    </div>
+  );
+}
+
+export default App;
